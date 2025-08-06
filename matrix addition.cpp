60cb34a9a2322d818fa49237355b38ce7c729008
Ishan2103 <ishanextra21@gmail.com>
2025-08-06 16:09:46 +0530

    Create matrix addition.cpp

---
+++ matrix addition.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r1, c1, r2, c2;
+
+    cout << "Enter r and c of first matrix: ";
+    cin >> r1 >> c1;
+
+    cout << "Enter r and c of second matrix: ";
+    cin >> r2 >> c2;
+
+    if (r1 != r2 || c1 != c2) {
+        cout << "Matrix addition not possible. Dimensions are not same." << endl;
+        return 0;
+    }
+
+    int a[10][10], b[10][10], sum[10][10];
+
+    cout << "Enter elements of first matrix:" << endl;
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    cout << "Enter elements of second matrix:" << endl;
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            cin >> b[i][j];
+        }
+    }
+    cout << "Sum of the matrices:" << endl;
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
